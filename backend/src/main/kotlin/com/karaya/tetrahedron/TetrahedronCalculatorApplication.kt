63a264b920c8fcf032bddef69dfaacc72f2538a2
Edwin Alt Karaya <chespirana@gmail.com>
2024-05-31 19:44:45 +0000

    Initial Commit

---
+++ backend/src/main/kotlin/com/karaya/tetrahedron/TetrahedronCalculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.karaya.tetrahedron
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TetrahedronCalculatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<TetrahedronCalculatorApplication>(*args)
+}
