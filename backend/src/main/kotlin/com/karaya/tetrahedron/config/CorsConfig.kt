eec15930991e1d72f3f584bf4d95517d66de02af
Edwin Alt Karaya <chespirana@gmail.com>
2024-06-02 05:07:20 +0000

    Fixing preflight CORS

--- backend/src/main/kotlin/com/karaya/tetrahedron/config/CorsConfig.kt
+++ backend/src/main/kotlin/com/karaya/tetrahedron/config/CorsConfig.kt
@@ -2,6 +2,9 @@ package com.karaya.tetrahedron.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -20,5 +23,18 @@ class CorsConfig {
                     .allowCredentials(true)
             }
         }
+    }
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = listOf("http://localhost:3000", "http://ae548371bb1254868840fa2c69e0e26c-139420865.us-west-2.elb.amazonaws.com")
+        config.allowedHeaders = listOf("*")
+        config.exposedHeaders = listOf("Location")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
     }
 }
