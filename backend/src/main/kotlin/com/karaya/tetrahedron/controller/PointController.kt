2ce60d521951eeafcb1b9a811d91597f5d68e7db
Edwin Alt Karaya <chespirana@gmail.com>
2024-06-02 06:12:44 +0000

    Trying response

--- backend/src/main/kotlin/com/karaya/tetrahedron/controller/PointController.kt
+++ backend/src/main/kotlin/com/karaya/tetrahedron/controller/PointController.kt
@@ -3,6 +3,7 @@ package com.karaya.tetrahedron.controller
 import com.karaya.tetrahedron.domain.Point
 import com.karaya.tetrahedron.service.PointSetService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
@@ -39,6 +40,7 @@ class PointController @Autowired constructor(
 
     @RequestMapping(method = [RequestMethod.OPTIONS])
     @CrossOrigin(origins = ["http://localhost:3000", "http://ae548371bb1254868840fa2c69e0e26c-139420865.us-west-2.elb.amazonaws.com"])
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun handleOptions() {
         // This method is intentionally left empty
     }
